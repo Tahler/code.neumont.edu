3ebaae326e74640622d8a6269bf866b564ad1202
Tyler Berry <tyler@berryac.com>
2016-09-13 17:00:09 -0600

    remove unused imports in verfication-required

--- src/app/verification-required/verification-required.module.ts
+++ src/app/verification-required/verification-required.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { VerificationRequiredComponent } from './verification-required.component';
 import { verificationRouting } from './verification-required.routing';
-import { NotVerifiedGuard } from '../shared';
 
 @NgModule({
   imports: [verificationRouting],
